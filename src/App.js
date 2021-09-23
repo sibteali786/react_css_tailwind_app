85b85637dbc92c5b22db70b3ffb9657532293066
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-23 08:45:59 +0500

    added some button components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import { Button } from "./components";
 
 function App() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div>
-        <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-gray-500">You have a new message!</p>
-      </div>
+    <div className="container p-4">
+      {/* <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-primary">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div> */}
+      <Button>Hello</Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "440px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "547px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "768px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1024px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1680px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#fb8500",
+        secondary: "#219ebc",
+      },
+    },
   },
   variants: {
     extend: {},
